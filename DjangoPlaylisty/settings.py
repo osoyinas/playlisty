a844924a83b05ba3aff234e34314c12b9aeab856
Pablo Piñas <osoyinas@gmail.com>
2022-12-29 17:14:39 +0100

    SESION ENGINE

--- DjangoPlaylisty/settings.py
+++ DjangoPlaylisty/settings.py
@@ -136,3 +136,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ['https://playlisty-production.up.railway.app']
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
