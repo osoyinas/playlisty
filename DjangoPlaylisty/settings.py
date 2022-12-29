83de40936fa811986d9c7d20a819545ce172b888
Pablo Piñas <osoyinas@gmail.com>
2022-12-29 01:18:52 +0100

    Config DB

--- DjangoPlaylisty/settings.py
+++ DjangoPlaylisty/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'DjangoPlaylisty.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'VBdqM1TQR9PdUuDIxvLG',
+        'HOST':'@containers-us-west-102.railway.app',
+        'PORT':'6982',
     }
 }
 
