44a6b8eebde7c70c0791fe00b50ea03ca2c8df33
Pablo Piñas <osoyinas@gmail.com>
2022-12-30 15:01:53 +0100

    DjangoSECRET

--- DjangoPlaylisty/settings.py
+++ DjangoPlaylisty/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-n(99rz2sc_pjjegza0tzb&(j8y!oi(l97ismjd1e_k1pxmt=co')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
