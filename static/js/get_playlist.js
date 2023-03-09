067031424860126cb7f06d6eff96674892f822ff
Pablo Piñas <osoyinas@gmail.com>
2023-03-09 16:49:19 +0100

    Add loader view when clicked generatePlaylist

--- static/js/get_playlist.js
+++ static/js/get_playlist.js
@@ -1,14 +1,16 @@
 const nameInput = document.getElementById("name_input");
 const submitButton = document.querySelector(".submit_button");
 const csrftoken = getCookie('csrftoken');
-
+const loader = document.querySelector(".loader");
+const section = document.getElementById("create_playlist");
 var idsList = []
 var data = { name: "", list: [] }
 var url = "/getplaylist/"
 
 
-submitButton.addEventListener("click", function() {
-    data.name = nameInput.value; 
+submitButton.addEventListener("click", function () {
+    data.name = nameInput.value;
+    loadingView();
     fetch(url, {
         method: 'POST',
         headers: {
@@ -17,14 +19,20 @@ submitButton.addEventListener("click", function() {
         },
         body: JSON.stringify(data)
     })
-        .then(response => response.json())
-        .then(jsonData=>{
+        .then(
+            response => response.json())
+        .then(jsonData => {
             window.location.href = jsonData.url;
         })
         .catch(error => {
             console.error('Error:', error);
         });
 });
+
+function loadingView() {
+    section.classList.add('hide');
+    loader.classList.add('show');
+}
 
 function getCookie(name) {
     let cookieValue = null;
