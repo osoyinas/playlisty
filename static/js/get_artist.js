f2bfe32db3f246f8093065df3f77fde33dd89929
Pablo Piñas <osoyinas@gmail.com>
2023-03-09 15:48:07 +0100

    Add get_playlist

--- static/js/get_artist.js
+++ static/js/get_artist.js
@@ -1,14 +1,22 @@
 const searchInput = document.getElementById('search');
 const searchWrapper = document.querySelector('.wrapper');
 const resultsWrapper = document.querySelector('.results');
-const submitForm = document.getElementById('playlist_form')
 const bottomContainer = document.querySelector('.bottom');
 const loggedIn = document.querySelector('.log_text').textContent != "Log in with Spotify!";
 const emptyContainer = document.getElementById('empty');
-const nameInput = document.getElementById("name_input");
-var idsList = []
-data = { name: "", list: [] }
-const listArtists = async (str) => {
+
+
+function addArtist(artistId) {
+    bottomContainer.innerHTML += `<iframe as="style" style="border-radius:12px" src="https://open.spotify.com/embed/artist/${artistId}" width="100%" height="80" frameBorder="0" allow="encrypted-media"></iframe>`;
+}
+
+
+function removeArtist(artistId) {
+    return;
+}
+
+
+async function listArtists (str) {
     try {
         if (str.length == 0) {
             searchWrapper.classList.remove('show');
@@ -24,7 +32,7 @@ const listArtists = async (str) => {
             if (data.message == "Success") {
                 content = ``;
                 data.artists.forEach(artist => {
-                    content += `<li><button onclick="optionClicked(event)" class="option" value="${artist.id}">${artist.name}</button></li>`
+                    content += artistResult(artist.id, artist.name)
                 });
                 resultsWrapper.innerHTML = `<ul>${content}</ul>`
             }
@@ -32,70 +40,35 @@ const listArtists = async (str) => {
     } catch (error) {
         console.log(error);
     }
-};
+}
 
-function optionClicked(event) {
-    artist_id = event.target.value;
-    artist_name = event.target.textContent;
+function artistResult(artistId, artistName){
+    return `<li><button onclick="optionClicked(event)" class="option" value="${artistId}">${artistName}</button></li>`
+}
+
+
+function optionClicked (event) {
+    artistId = event.target.value;
+    artistName = event.target.textContent;
     searchInput.value = ``;
     resultsWrapper.innerHTML = ``;
     searchWrapper.classList.remove('show');
-    if (data.list.includes(artist_id)) {
+    if (data.list.includes(artistId)) {
         return;
     }
-    data.list.push(artist_id);
-    bottomContainer.innerHTML += `<iframe as="style" style="border-radius:12px" src="https://open.spotify.com/embed/artist/${artist_id}" width="100%" height="80" frameBorder="0" allow="encrypted-media"></iframe>`;
+    data.list.push(artistId); //save the artist id 
+    addArtist(artistId);
 }
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
 
-const csrftoken = getCookie('csrftoken');
-
-submitForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    data.name = nameInput.value;
-    let url = "/generateplaylist/"
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrftoken
-
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(jsonData=>{
-            // Redirect to the requested page
-            console.log(jsonData);
-            window.location.href = jsonData.url;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-})
-
-
-const waitInit = async () => {
+async function  waitInit() {
+    //every time the user types a letter, searchs for coincidence
     searchInput.addEventListener('keyup', (event) => {
         let input = searchInput.value;
         listArtists(input);
     })
-};
+}
+
 
 window.addEventListener("load", async () => {
     await waitInit();
---
+++ static/js/get_playlist.js
@@ -0,0 +1,43 @@
+const nameInput = document.getElementById("name_input");
+const submitForm = document.getElementById('playlist_form');
+const csrftoken = getCookie('csrftoken');
+
+var idsList = []
+var data = { name: "", list: [] }
+var url = "/generateplaylist/"
+
+submitForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    data.name = nameInput.value; 
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(jsonData=>{
+            window.location.href = jsonData.url;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+})
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
