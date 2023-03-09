109869f1b013fe27e5b3efcd68203ee9c42df3e3
Pablo Piñas <osoyinas@gmail.com>
2023-03-09 15:11:53 +0100

    Feat: fetch generateplaylist

--- static/js/get_artist.js
+++ static/js/get_artist.js
@@ -1,12 +1,12 @@
 const searchInput = document.getElementById('search');
 const searchWrapper = document.querySelector('.wrapper');
 const resultsWrapper = document.querySelector('.results');
-const hiddenInput = document.getElementById("list_ids")
 const submitForm = document.getElementById('playlist_form')
 const bottomContainer = document.querySelector('.bottom');
-const loggedIn = document.querySelector('.log_text').textContent != "Log in with Spotify!"
-const emptyContainer = document.getElementById('empty')
-var list_id = []
+const loggedIn = document.querySelector('.log_text').textContent != "Log in with Spotify!";
+const emptyContainer = document.getElementById('empty');
+const nameInput = document.getElementById("name_input");
+var idsList = []
 data = { name: "", list: [] }
 const listArtists = async (str) => {
     try {
@@ -47,20 +47,42 @@ function optionClicked(event) {
     bottomContainer.innerHTML += `<iframe as="style" style="border-radius:12px" src="https://open.spotify.com/embed/artist/${artist_id}" width="100%" height="80" frameBorder="0" allow="encrypted-media"></iframe>`;
 }
 
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
 submitForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    data.name = document.getElementById("input_name").value;
-    let url = "/generateplaylist"
+    data.name = nameInput.value;
+    let url = "/generateplaylist/"
     fetch(url, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+
         },
         body: JSON.stringify(data)
     })
-        .then(response => {
+        .then(response => response.json())
+        .then(jsonData=>{
             // Redirect to the requested page
-            window.location.href = response.url;
+            console.log(jsonData);
+            window.location.href = jsonData.url;
         })
         .catch(error => {
             console.error('Error:', error);
