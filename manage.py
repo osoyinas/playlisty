47960b6e812a5c001cdde00327ab35b77f3ff190
Pablo Piñas <osoyinas@gmail.com>
2023-05-26 14:34:31 +0200

    Manage dotenv exception

--- manage.py
+++ manage.py
@@ -6,10 +6,10 @@ import dotenv
 
 def main():
     """Run administrative tasks."""
-    dotenv.read_dotenv()
-    DEBUG = str(os.environ.get('DEBUG')) == "1"
-    if (DEBUG):
+    try:
         dotenv.read_dotenv()
+    except:
+        print("no dotenv")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoPlaylisty.settings')
     try:
         from django.core.management import execute_from_command_line
