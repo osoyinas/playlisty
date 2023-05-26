e927cbec4a472a3c9bf6c47097ba99914bd57a9a
Pablo Piñas <osoyinas@gmail.com>
2023-05-26 12:51:37 +0200

    Change templates dir

--- DjangoPlaylisty/settings.py
+++ DjangoPlaylisty/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / "templates",
+            BASE_DIR / "DjangoPlaylisty" / "templates",
         ],
         'APP_DIRS': True,
         'OPTIONS': {
