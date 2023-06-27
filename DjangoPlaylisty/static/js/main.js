9a8ff51ec425b1ddb3a75e9df121fb5445e00325
osoyinas <osoyinas@gmail.com>
2023-06-27 23:56:21 +0200

    Add delete button

--- DjangoPlaylisty/static/js/main.js
+++ DjangoPlaylisty/static/js/main.js
@@ -86,7 +86,6 @@ function addItemToPlaylistContainer(name, id, type, image) {
     let content =
         `<li id-value="${id}" type-value="${type}">
             <div class="left">
-                <h2>${count}</h2>
             <img src="${image}" alt="">
             <div class="name-type-container">
                 <p class="name">${name}</p>
@@ -102,6 +101,9 @@ function addItemToPlaylistContainer(name, id, type, image) {
                 <option value="top-tracks">top 10 tracks</option>
                 <option value="all-tracks">all tracks</option>
               </select>
+              <button class="button" onclick="deleteElement(this)">
+              <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>
+            </button>
             </div>`;
             break;
 
@@ -173,3 +175,11 @@ function getCurrentType() {
 selectMenu.addEventListener('change', function (event) {
     startTimerAndFetch();
 });
+
+function deleteElement(button) {
+    // Obtén el elemento <li> que contiene el botón
+    var listItem = button.closest("li");
+    
+    // Elimina el elemento <li> de la lista
+    listItem.remove();
+}
