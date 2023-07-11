481819330bb04aa54b6729c56cc202bf66823de6
Pablo Piñas <osoyinas@gmail.com>
2023-07-11 20:48:05 +0200

    Change create playlist design

--- DjangoPlaylisty/static/js/create-playlist.js
+++ DjangoPlaylisty/static/js/create-playlist.js
@@ -11,7 +11,13 @@ const emptyPlaylist = document.querySelector('.playlist-empty')
 var items_ids = []  
 var count = 0; //playlist items count
 var timer = null //timer to delay the requests
-
+var artistsNumber = 0
+var albumsNumber = 0
+var tracksNumber = 0
+const domArtistsNumber = document.getElementById('artists-n')
+const domAlbumsNumber = document.getElementById('albums-n')
+const domTracksNumber = document.getElementById('tracks-n')
+const domTotalNumber = document.getElementById('total-n')
 //User typed in search input
 searchInput.addEventListener('input', function (event) {
     startTimerAndFetch();
@@ -85,7 +91,7 @@ function addToResultsItems(result) {
 }
 //Adds an item to the playlist container with its own options depending on the item
 function addItemToPlaylistContainer(name, id, type, image, url) {
-    emptyPlaylist.classList.add('hide')
+    // emptyPlaylist.classList.add('hide')
     if (items_ids.includes(id)){
         return;
     }
@@ -103,6 +109,8 @@ function addItemToPlaylistContainer(name, id, type, image, url) {
     `
     switch (type) {
         case "artist":
+            artistsNumber+=1
+            domArtistsNumber.textContent = artistsNumber
             content +=
                 `<div class="right">
               <select name="options">
@@ -112,6 +120,8 @@ function addItemToPlaylistContainer(name, id, type, image, url) {
             break;
 
         case "track":
+            tracksNumber+=1
+            domTracksNumber.textContent = tracksNumber
             content +=
                 `<div class="right">
               <select name="options">
@@ -121,6 +131,8 @@ function addItemToPlaylistContainer(name, id, type, image, url) {
             break;
 
         case "album":
+            albumsNumber+=1
+            domAlbumsNumber.textContent = albumsNumber
             content +=
                 `<div class="right">
               <select name="options">
@@ -134,11 +146,12 @@ function addItemToPlaylistContainer(name, id, type, image, url) {
                     <button class="button" onclick="deleteElement(this)">
                     <svg viewBox="0 0 448 512" class="svgIcon"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path></svg>
                 </button>
-                <a class="right-icon" href="${url}" target="_blank" rel="noreferrer noopener" alt="${name} on Spotify"><img src="/static/svg/Spotify_Icon_RGB_White.png" alt=""></a>
                
                 </div>
                 </li>
                 `
+                //<a class="right-icon" href="${url}" target="_blank" rel="noreferrer noopener" alt="${name} on Spotify"><img src="/static/svg/Spotify_Icon_RGB_Black.png" alt=""></a>
+    domTotalNumber.textContent = albumsNumber + tracksNumber + artistsNumber
     playlistContainer.innerHTML += content;
     var item = document.querySelector('.playlist-container li[id-value="' + id + '"]');
     setTimeout(() => {
@@ -214,7 +227,7 @@ function deleteElement(button) {
     setTimeout(() => {
         listItem.remove();
         if (items_ids.length == 0){
-            emptyPlaylist.classList.remove('hide')
+            // emptyPlaylist.classList.remove('hide')
         }
     }, 400);
 
