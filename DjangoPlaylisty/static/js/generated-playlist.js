43da2aad4ed0252f0e21c0838e29ae4e7393070e
Pablo Piñas <osoyinas@gmail.com>
2023-07-14 14:48:15 +0200

    Change iframe background

--- DjangoPlaylisty/static/js/generated-playlist.js
+++ DjangoPlaylisty/static/js/generated-playlist.js
@@ -1,10 +1,10 @@
 const buttons = document.querySelectorAll('button')
 
 buttons.forEach((button) => {
-    console.log(button);
 
     button.addEventListener('click', () => {
         const url = button.getAttribute('url-data')
         window.location.href = url
     })
+
 })
