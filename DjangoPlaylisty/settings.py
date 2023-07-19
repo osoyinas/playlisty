4ec9f4da7ef8d3dbfd473118937ed8e19699e600
Pablo Piñas <osoyinas@gmail.com>
2023-07-20 00:03:34 +0200

    Fix trusted origins

--- DjangoPlaylisty/settings.py
+++ DjangoPlaylisty/settings.py
@@ -142,8 +142,8 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CSRF_TRUSTED_ORIGINS = [os.environ.get("HOST_URL"), os.environ.get("HOST_URL")]
+CSRF_TRUSTED_ORIGINS = (str)[os.environ.get("HOST_URL"), "https://playlisty.es"]
 
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get("HOST_URL"),
+    "https://playlisty.es",
 ]
