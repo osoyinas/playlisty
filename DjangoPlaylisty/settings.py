75d20fc7a2f57035d1dade4adff01305ffc5d6b1
Pablo Piñas <osoyinas@gmail.com>
2023-07-20 00:09:27 +0200

    Remove print

--- DjangoPlaylisty/settings.py
+++ DjangoPlaylisty/settings.py
@@ -144,7 +144,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CSRF_TRUSTED_ORIGINS = [os.environ.get("HOST_URL"), "https://playlisty.es"]
 HOST_URL = os.environ.get("HOST_URL")
-print(HOST_URL)
 CORS_ALLOWED_ORIGINS = [
     "https://playlisty.es",
 ]
