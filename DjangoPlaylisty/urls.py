4d4749b75353d1f44671bdfad02c1d040bf41c37
Pablo Piñas <osoyinas@gmail.com>
2023-07-24 11:36:47 +0200

    Refactor and doc

--- DjangoPlaylisty/urls.py
+++ DjangoPlaylisty/urls.py
@@ -1,3 +1,7 @@
+"""
+Django project urls.
+
+"""
 from django.contrib import admin
 from django.urls import path, include
 from . import views
--- DjangoPlaylisty/views.py
+++ DjangoPlaylisty/views.py
@@ -1,3 +1,7 @@
+"""
+Django home views.
+
+"""
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from playlistGeneration.api.spotify import check_logged_in
@@ -8,29 +12,31 @@ def home(request: HttpRequest) -> HttpResponse:
     Index page view. Checks if the user is logged in and passes that information to the template.
     """
 
-    set_prepath(request)
     logged_in = check_logged_in(request)
     # store the current page
-    set_prepath(request)
     context = {"logged_in": logged_in}
     return render(request, "home.html", context)
 
 
 def not_white_listed(request: HttpRequest):
+    """not_white_listed
+
+    Args:
+        request (HttpRequest): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return render(request, "not_whitelisted.html")
 
 
 def why_login(request: HttpRequest):
-    return render(request, "why_log_in.html")
-
-# Aux functions
-
-
-def set_prepath(request: HttpRequest):
-    """
-    Saves the current web page to control redirects. If i am in /createplaylist and i logout, I will be redirected to /createplaylist
+    """why_login
 
     Args:
-        request (HttpRequest): request
+        request (HttpRequest): _description_
+
+    Returns:
+        _type_: _description_
     """
-    request.session["pre_path"] = request.resolver_match.url_name
+    return render(request, "why_log_in.html")
