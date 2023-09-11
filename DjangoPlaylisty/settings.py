1dd3d24b11dc496a880959ff2ad586d960af6b2a
Pablo Piñas <osoyinas@gmail.com>
2023-09-11 16:31:32 +0200

    Remove CSRF and CORS

--- DjangoPlaylisty/settings.py
+++ DjangoPlaylisty/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -100,9 +99,6 @@ else:
     }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -119,9 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -131,22 +124,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "DjangoPlaylisty" / "static",
     BASE_DIR / "playlistGeneration" / "static",
 ]
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CSRF_TRUSTED_ORIGINS = [os.environ.get("HOST_URL"), "https://playlisty.es"]
-HOST_URL = os.environ.get("HOST_URL")
-CORS_ALLOWED_ORIGINS = [
-    "https://playlisty.es",
-]
+HOST_URL = os.environ.get("RAILWAY_PUBLIC_DOMAIN")
